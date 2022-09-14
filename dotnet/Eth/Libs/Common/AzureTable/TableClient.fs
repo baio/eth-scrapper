2ee7325cdd4b6c45431297395a426ab4fb3037b4
maxxx80 <maxjvc11+1@gmail.com>
2022-09-14 22:46:53 +0300

    cleanup

--- dotnet/Eth/Libs/Common/AzureTable/TableClient.fs
+++
@@ -1,8 +0,0 @@
-namespace Infra.AzureTable
-
-[<AutoOpen>]
-module TableClient =
-    open Azure.Data.Tables
-
-    let azureTableClient (connectionString: string, tableName: string) =
-        lazy (TableClient(connectionString, tableName))
--- dotnet/Eth/StoreReaderActor/readme.md
+++
@@ -1,3 +0,0 @@
-$Env:PORT=5004
-
-dapr run --app-port $Env:Port --app-id store-reader --components-path ../../../components --dapr-http-port 3504 -- dotnet watch run
